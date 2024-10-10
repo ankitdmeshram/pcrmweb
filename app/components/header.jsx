038b9efb7bdbdc5d5bd3f2379b1242181cb9572f
Ankit Meshram <ankitdm69@gmail.com>
2024-10-10 11:22:19 +0530

    Removed logout button from top navbar and add it to the sidebar

--- app/components/header.jsx
+++ app/components/header.jsx
@@ -17,9 +17,6 @@ const header = ({ setSidebarOpen, sidebarOpen }) => {
         <div className="header  bg-white shadow-sm">
             <div className="logo ps-5 pe-3">PCRM <i className="fa-solid fa-bars" onClick={() => setSidebarOpen(!sidebarOpen)}></i></div>
             <div className="pageName ps-4"></div>
-            <div className="header-nav-links pe-5">
-                <div className="logout gs-link" onClick={() => handleLogout()}>Logout</div>
-            </div>
         </div>
     )
 }
--- app/components/sidebar.jsx
+++ app/components/sidebar.jsx
@@ -1,16 +1,28 @@
 import { useNavigate } from "@remix-run/react"
+import { signOut } from "../controllers/auth"
 
 const sidebar = ({ sidebarOpen }) => {
     const navigate = useNavigate()
+
+    const handleLogout = () => {
+        try {
+            if (signOut() == true) {
+                navigate("/")
+            } else {
+                console.log("Error logging out")
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
     return (
         <div className={sidebarOpen ? "sidebar bg-white shadow-sm sideBarOpen" : "sidebar bg-white shadow-sm"}>
             <div className="sidebar-links">
                 <div className="sidebar-link" onClick={() => navigate("../dashboard")}><i className="fa-solid fa-house"></i> Home</div>
                 <div className="sidebar-link" onClick={() => navigate("../projects")}><i className="fa-solid fa-briefcase"></i> Projects</div>
-                {/* <div className="sidebar-link"><i className="fa-solid fa-file-invoice-dollar"></i> Billings</div>
-                <div className="sidebar-link"><i className="fa-solid fa-users"></i> Customers</div>
-                <div className="sidebar-link"><i className="fa-solid fa-gear"></i> Settings</div> */}
                 <div className="sidebar-link" onClick={() => navigate("../my-account")}><i className="fa-solid fa-user"></i> My Account</div>
+                <div className="sidebar-link" onClick={() => handleLogout()}><i class="fa fa-sign-out" aria-hidden="true"></i> Logout</div>
             </div>
         </div>
     )
